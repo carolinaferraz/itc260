bb5c58506734bf47a7ed5daffadd6334efd95521
carolinaferraz <43863678+carolinaferraz@users.noreply.github.com>
2019-04-24 14:26:32 -0700

    updates nav with makeLinks()

--- application/views/themes/bootswatch/header.php
+++ application/views/themes/bootswatch/header.php
@@ -1,3 +1,5 @@
+<? //views/themes/bootswatch/header.php
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -24,9 +26,7 @@
         </div>
         <div class="navbar-collapse collapse" id="navbar-main">
           <ul class="nav navbar-nav">
-			<li class="active"><a href="#" title="">Active Link</a></li>
-			<li><?=anchor('customer/mylist','Customers')?></li>
-			<li><?=anchor('customer/add','Add Customer')?></li>
+          <?=makeLinks($this->config->item('nav1'))?>
           </ul>
         </div>
       </div>
