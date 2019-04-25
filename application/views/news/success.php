69638598d54c483f749e1ef8ad414e6e0e096ae0
cferraz <carolinapferraz@gmain.com>
2019-04-25 16:59:01 -0700

    redoing github desktop connection
    + technical issues

--- application/views/news/success.php
+++
@@ -1,5 +0,0 @@
-<?php
-//application/views/news/success.php
-?>
-<h1><span style="background-color: #fff5f5">&nbsp;Success!&nbsp;</span></h1>
-<p>wouldn't it be awesome if I could see the new record?</p>
